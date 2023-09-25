1e237c7c63ee9f2bdd50fa628a1a23d8b817e67d
stanmpakati <stanmpakatig@gmail.com>
2023-09-25 19:31:18 +0200

    created service discovery (eureka) server

---
+++ backend/service-registry/src/test/java/com/stancloud/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stancloud.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
